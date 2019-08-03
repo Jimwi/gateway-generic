3f39569c21a8bb3871c3a6fc821dc4e066cef8b1
chenjiwen <chenjiwen@qudian.com>
2019-08-03 12:23:47 +0800

    优化

--- gateway-common/src/main/java/com/example/gateway/common/util/crypto/MD5Util.java
+++ gateway-common/src/main/java/com/example/gateway/common/util/crypto/MD5Util.java
@@ -1,11 +1,10 @@
 package com.example.gateway.common.util.crypto;
 
 
-import java.nio.charset.Charset;
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
-
-import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  * Created by JiWen on 2019/7/2 at home.
@@ -13,18 +12,16 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class MD5Util {
 
-    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
-
     public static String sign(String data, String key) {
         if (data == null) data = "";
         if (key == null) key = "";
-        return DigestUtils.md5Hex((data + key).getBytes(DEFAULT_CHARSET));
+        return DigestUtils.md5Hex((data + key).getBytes(StandardCharsets.UTF_8));
     }
 
     public static boolean verify(String data, String sign, String key) {
         if (data == null) data = "";
         if (key == null) key = "";
-        return Objects.equals(sign, DigestUtils.md5Hex((data + key).getBytes(DEFAULT_CHARSET)));
+        return Objects.equals(sign, DigestUtils.md5Hex((data + key).getBytes(StandardCharsets.UTF_8)));
     }
 
 }
